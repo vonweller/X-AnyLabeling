0482322dd515aa9206f7595ec66a1c81a73254f1
vonweller <154695311+vonweller@users.noreply.github.com>
2025-06-01 17:37:23 +0800

    添加查询cuda脚本

---
+++ 查询.py
@@ -0,0 +1,7 @@
+import torch
+import torchvision
+import torchaudio
+
+print("Torch version:", torch.__version__)
+print("Torchvision version:", torchvision.__version__)
+print("Torchaudio version:", torchaudio.__version__)
